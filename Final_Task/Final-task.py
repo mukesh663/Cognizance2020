6ef72e4631b7f858b294ceafb3f1abaa84e57545
Mukesh <mukesh13034@gmail.com>
2020-12-20 22:27:52 +0530

    New commit

--- Final_Task/Final-task.py
+++ Final_Task/Final-task.py
@@ -1,17 +1,19 @@
 from __future__ import print_function
-from logging import logMultiprocessing
 import pickle
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-import datetime, itertools
+import datetime
+import sqlite3
+
+conn=sqlite3.connect('Student.db')
+c=conn.cursor()
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/spreadsheets']
-
-
-def main():
+def get_service():
     creds = None
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
@@ -27,69 +29,57 @@ def main():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    gmailservice=build('gmail', 'v1', credentials=creds)
-    sheetservice=build('sheets', 'v4', credentials=creds)
-    
-    # Starting of main code part 
-    a=str(input(print("Do you want to create a new spreadsheet(y/n):")))
+    service=build('gmail', 'v1', credentials=creds)
+    return service
 
-    if a == ('y'):
-        spreadsheet = {'properties': {'title': 'Student_list'}}   #you can also give your own name for sheet
-        spreadsheet = sheetservice.spreadsheets().create(body=spreadsheet,fields='spreadsheetId').execute()
-        SPREADSHEET_ID1 = spreadsheet.get('spreadsheetId')
+# Starting of main code part 
+def main():
+
+    a=str(input("Do you want to enter values into Student-List table(y/n):"))
+    if a==('y'):
+        b=int(input("Enter number of students:"))
+        c.execute("CREATE TABLE IF NOT EXISTS StudentList(Name TEXT)")
+        for i in range(0,b):
+            name=str(input("Enter name:"))  
+            c.execute( "INSERT INTO StudentList VALUES(?)", (name,))
+            conn.commit()
     else:
-        SPREADSHEET_ID1 = input("Enter the spreadsheet Id:")
-    
-    b=str(input("Do you want to enter names in spreadsheet (y/n):"))
-    if b==('y'):
-        values = [['Name']] 
-        c=int(input("enter number of students:"))
-        for i in range(0,c):
-            d=str(input("Enter name:"))
-            values.append([d])
-        print(values)
-        body = {'values': values }    # to update spreadsheet using python code
-        result = sheetservice.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID1, range='A1:A',valueInputOption='RAW', body=body).execute()
-        print('{0} cells updated.'.format(result.get('updatedCells')))
-    else:
-        print("ok")
-    
-    # accessing the threads from mail and storing it in spread sheet
-    results = gmailservice.users().messages().list(userId='me', labelIds='INBOX').execute()
+        print('ok') 
+
+    # accessing the threads from mail 
+    results = get_service().users().messages().list(userId='me', labelIds='INBOX').execute()
     messages = results.get('messages',[])
-    e=int(input("How many emails to access:"))
-    
-    values2=[['Name','Timestamp','Content']]    
-    for message in messages[0:e]:
-     msg = gmailservice.users().messages().get(userId='me', id=message['id']).execute()
-     time=int(msg['internalDate'])
-     timestamp=datetime.datetime.fromtimestamp(time/1000)      # it's in epoch time so we are converting it using datetime module
-     name=msg['payload']['headers']
-     for i in range(0,len(name)):
-        if name[i]['name']=='From':                  # getting the From address of the mail from headers field
-            mailname=name[i]['value']
-            break
-     content=msg['snippet']                           # subject of the mail can also be found but it makes the program slow
-     values2.append([mailname,str(timestamp),content])
-    
-    f=str(input("Do you want to create a new spreadsheet for storing mail content(y/n):"))
-    if f==('y'):
-      spreadsheet = {'properties': {'title': 'Mail_list'}}
-      spreadsheet = sheetservice.spreadsheets().create(body=spreadsheet,fields='spreadsheetId').execute()
-      SPREADSHEET_ID2 = spreadsheet.get('spreadsheetId')
+    inp=str(input("Do you want to enter the mail contents in table(y/n):"))
+    if inp=='y':
+      d=int(input("How many emails to access:"))
+      values=[]
+      for message in messages[0:d]:
+         msg = get_service().users().messages().get(userId='me', id=message['id']).execute()
+         time=int(msg['internalDate'])
+         timestamp=datetime.datetime.fromtimestamp(time/1000)      # it's in epoch time so we are converting it using datetime module
+         name=msg['payload']['headers']
+         for i in range(0,len(name)):
+             if name[i]['name']=='From':                  # getting the From address of the mail from headers field
+                 mailname=name[i]['value']
+                 break
+         cont=msg['payload']['headers']
+         for i in range(0,len(name)):
+             if cont[i]['name']=='Subject':                  # getting the From address of the mail from headers field
+                 content=name[i]['value']
+                 break            
+         values.append((mailname,str(timestamp),content))
+      c.execute("CREATE TABLE IF NOT EXISTS MailList(Name TEXT,Timestamp DATETIME, Content TEXT)")
+      for row in values:
+         print(row)
+         c.execute( "INSERT INTO MailList VALUES(?, ?, ?)", row)
+         conn.commit()
     else:
-        SPREADSHEET_ID2=input("Enter spreadsheet Id:")
+        print('ok')
 
-    body2 = {'values': values2 } 
-    result2 = sheetservice.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID2, range='A1:C',valueInputOption='RAW', body=body2).execute()
-    print('{0} cells updated.'.format(result2.get('updatedCells')))
-
-    compare1 = sheetservice.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID1, range='A2:A').execute()
-    compare2 = sheetservice.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID2, range='A2:A').execute()
-    
-    # the values of compare1 and 2 are stored as list inside another list(nested list), so we are extracting it using itertools module
-    final_result= set(itertools.chain(*compare1['values']))-set(itertools.chain(*compare2['values']))   
-    print("The students who have not mailed:",final_result)    # set data type is used for omitting duplicate entries
-
+    c.execute("SELECT Name FROM StudentList WHERE Name NOT IN (SELECT Name FROM MailList)")
+    final_result=c.fetchall()
+    print("The students who have not mailed:",final_result)    
+    c.close()
+    conn.close()
 if __name__ == '__main__':
     main()
--- Final_Task/README.md
+++ Final_Task/README.md
@@ -1,16 +1,14 @@
 # About the program
-<p>The program fetches mails from your gmail account and creates a Google spreadsheet whose columns include Name, Mail content and Timestamp.</br>
-It allows you to create a new spreadsheet or enter an existing spreadsheet Id consisting of students name
+<p>The program fetches mails from your gmail account and creates a SQL table whose columns include Name, Mail content and Timestamp.</br>
+It allows you to create a new table or continue with an existing table consisting of students name
 and compares with the spreadsheet extracted fom your mail, and gives back the names of the students who have not mailed. </p>
 
 > ## Pre-requisites for the program to run
-1. Before running the program you must have enabled Google API and must have saved the credentials.json file in your current working directory
-2. If you have not done it, then you can visit the following site, which also contains the procedure to enable Google API.</br>
-[API site link](https://developers.google.com/gmail/api/quickstart/python)
-3. Check whether you have enabled API in API console, the following link will lead to the console.</br>
-[Google API console](https://console.developers.google.com/apis/dashboard?project=quickstart-1606870604894&folder=&organizationId=)
-4. When you try to run the program for first time it would ask you, access to your google account.
-5. Now you are good to go.
+You should have installed DB browser for SQlite
+  or sqlite3 from internet.<br>
+  I have also include a link for downloading DB browser.<br>
+  [DB browser installation link](https://sqlitebrowser.org/blog/version-3-12-1-released/) 
+ 
 
 
 
